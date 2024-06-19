2d78971e5558c8272162230ba09cc0acd2b02499
dandok <54486520+dandok@users.noreply.github.com>
2024-06-19 08:13:23 +0100

    code cleanup - removed unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { dataSourceOptions } from './db/data-source';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionModule } from './transaction/transaction.module';
 import { SeedsModule } from './db/seeds/seeds.module';
-import { AppService } from './app.service';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { GlobalExceptionsFilter } from './helpers/exceptionFilter.helpers';
 import { AuthModule } from './auth/auth.module';
@@ -23,9 +21,7 @@ import { JwtModule } from '@nestjs/jwt';
     SeedsModule,
     JwtModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_FILTER,
       useClass: GlobalExceptionsFilter,
